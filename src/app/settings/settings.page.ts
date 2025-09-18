ed849d2ead52c441d22837903db5887c87c0e06c
vintalie <marioluizsan@gmail.com>
2025-09-18 02:24:42 -0300

    controlador que recebe as informações e repassa para o localstorage, aqui é definido 2 variaveis primarias para definir o valor inicial e tipo inicial caso já tenham sido definidas anteriormente.
    a função save, como o proprio nome diz, aloca os dados no localstorage, também existe um navCtrl para voltar a pagina inicial quando submetido ao formulario

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
+  standalone:false
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage {
+  userName: string = '';
+  selectedColor: string = 'white';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    const name = localStorage.getItem('userName');
+    const color = localStorage.getItem('backgroundColor');
+    
+    if (name) this.userName = name;
+    if (color) this.selectedColor = color;
   }
 
+  save() {
+    localStorage.setItem('userName', this.userName);
+    localStorage.setItem('backgroundColor', this.selectedColor);
+    this.navCtrl.back();
+  }
+
+  cancel() {
+    this.navCtrl.back();
+  }
 }
