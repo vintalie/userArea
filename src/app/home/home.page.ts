f39881b3c8da15b1d1d5cd03038a0c548324bdca
vintalie <marioluizsan@gmail.com>
2025-09-18 02:04:21 -0300

    Iniciando projeto para atividade da prof eulaliane

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone:false
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  userName: string = 'Usuário';
+  backgroundColor: string = 'white';
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.loadUserData();
+  }
+
+  loadUserData() {
+    const name = localStorage.getItem('userName');
+    const color = localStorage.getItem('backgroundColor');
+    
+    if (name) this.userName = name;
+    if (color) this.backgroundColor = color;
+  }
+
+  openSettings() {
+    this.router.navigate(['/settings']);
+  }
 }
